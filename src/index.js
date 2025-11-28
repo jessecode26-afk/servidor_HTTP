aaa035360caebac32ba8bee638eb530772468fe4
Jessé Silva <jessecode26@gmail.com>
2025-11-27 22:33:05 -0300

    feat: implementa rota GET /users com retorno em JSON e resposta padrão para endpoints não encontrados

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-const http = require('http');
+const http = require("http");
 
-const server = http.createServer(( resquest, response) => {
-   response.writeHead(200, {'content-type': 'text/html'});
-   response.end('<h1> Hello World! </h1>');
+const users = require('./mocks/users');
+
+const server = http.createServer((request, response) => {
+  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+
+  if (request.url === "/users" && request.method === "GET") {
+    response.writeHead(200, { "content-type": "application/json" });
+    response.end(JSON.stringify(users));
+  } else {
+
+   response.writeHead(401, {'content-type': 'text/html'});
+   response.end(`Cannot ${request.method} ${request.url}`);
+  }
+
 });
 
-server.listen(3000, () => console.log('🔥 server started at http://localhost:3000'));
+server.listen(3000, () =>
+  console.log("🔥 server started at http://localhost:3000")
+);
---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Jessé',
+  },  
+  {
+    id: 2,
+    name: 'Davi',
+  },  
+  {
+    id: 3,
+    name: 'Dalila',
+  },
+];
